b1e144a6a186ea9ce2e791078935203a3c9055f3
AKTMB <aktmb.ak@gmail.com>
2017-01-27 08:56:26 -0700

    More Adjustments
    No errors, but still not working right

--- app.js
+++ app.js
@@ -1,83 +1,5 @@
-/*
-// Single state object
 var state = {
-    items: []
-};
-
-// State modification functions
-var addItem = function(state, item) {
-    state.items.push(item);
-};
-
-// Render functions
-var renderList = function(state, element) {
-    var itemsHTML = state.items.map(function(item) {
-        return '<li>' + item + '</li>';
-    });
-    element.html(itemsHTML);
-};
-
-// Event listeners
-$('.js-shopping-list-form1').submit(function(event) {
-    event.preventDefault();
-    addItem(state, $('.shopping-list-entry').val());
-    renderList(state, $('.shopping-list'));
-});
-
-
-
-// Single state object
-var state = {
-    items: []
-};
-
-// State modification functions
-var addItem = function(state, item) {
-    state.items.push(item);
-};
-
-// Setup repeat layout
-var listItemTemplate = (
-  '<li>' +
-    '<span class="shopping-item js-shopping-item"></span>' +
-    '<div class="shopping-item-controls">' +
-      '<button class="js-shopping-item-toggle">' +
-        '<span class="button-label">check</span>' +
-      '</button>' +
-      '<button class="js-shopping-item-delete">' +
-        '<span class="button-label">delete</span>' +
-      '</button>' +
-    '</div>' +
-  '</li>'
-);
-
-// Render functions
-var renderList = function(state, element) {
-    var itemsHTML = state.items.map(function(item) {
-        return listItemTemplate('.js-shopping-item'(item));
-    });
-    element.html(itemsHTML);
-};
-
-// Event listeners
-$('.shopping-list-add').submit(function(event) {
-    event.preventDefault();
-    addItem(state, $('.shopping-list-add-input').val());
-    renderList(state, $('.shopping-list'));
-});
-
-
- var cart = [];
-
- var item = {name:"apple", cost:1.99, count:3};
- cart.push(item);
- console.log(cart);
- console.log(cart[0]);
- console.log(cart[0].name);
-*/
-
-var list = {
-  items:[]
+  items: []
 };
 
 var listTemplate = (
@@ -94,29 +16,29 @@ var listTemplate = (
   '</li>'
 );
 
-function addItem(list,item) {
-  list.items.push({
+function addItem(state,item) {
+  state.items.push({
      displayName: item,
      checkedOff: false,
   });
 }
 
-function getItem(list,itemIndex) {
-  return list.items[itemIndex];
+function getItem(state,itemIndex) {
+  return state.items[itemIndex];
 }
 
-function deleteItem(list, itenIndex) {
-  list.items.splice(itemIndex, 1);
+function deleteItem(state, itenIndex) {
+  state.items.splice(itemIndex, 1);
 }
 
-function updateItem(list,itemIndex, updatedItem) {
-  list.items[itemIndex] = updatedItem;
+function updateItem(state,itemIndex, updatedItem) {
+  state.items[itemIndex] = updatedItem;
 }
 
 //HTML Changers
 
-function makeItem(item, itemName,itemTemplate, itemInfo) {
-  var element = $(itemTemplate);
+function makeItem(item, itemName, listTemplate, itemInfo) {
+  var element = $(listTemplate);
   element.find(".js-shopping-item").text(item.displayName);
   if (item.checkedOff) {
     element.find(".js-shopping-item").addClass(".shopping-item__checked");
@@ -126,58 +48,196 @@ function makeItem(item, itemName,itemTemplate, itemInfo) {
   return element;
 }
 
-function makeList(list, listTemplate, itemInfo) {
-  var itemsHTML = list.items.map(
+function makeList(state, listElemnt, itemInfo) {
+  var itemsHTML = state.items.map(
     function(item, index) {
-      return makeItem(item, index, listItemTemplate, itemInfo);
+      return makeItem(item, index, listTemplate, itemInfo);
     });
-  listTemplate.html(itemsHTML);
+  listElemnt.html(itemsHTML);
 }
 
 //Event Watchers
 
-function handleAdd (formElement, newItemId, itemTemplate, list) {
+function handleAdd (formElement, newItemId, itemInfo, listElemnt, state) {
   formElement.submit(function(event) {
   event.preventDefault();
+  console.log("Hello");
   var newItem = formElement.find(newItemId).val();
-  addItem(list,newItem);
-  makeList(list, listTemplate, itemInfo);
+  //console.log(newItem);
+  addItem(state,newItem);
+  makeList(state, listElemnt, itemInfo);
   this.reset();
   });
 }
 
-function handleDelete (formElement, removeId, itemInfo, listTemplate, list){
-  listTemplate.on("click", removeId, function(event){
+function handleDelete (formElement, removeId, itemInfo, listElemnt, state){
+  listElemnt.on("click", removeId, function(event){
     var itemIndex = parseInt($(this).closest("li").attr(itemInfo));
     deleteItem(list, itemIndex);
-    makeList(listTemplate,itemInfo);
+    makeList(list, listElemnt, itemInfo);
   })
 }
 
-function handleToggle (listTemplate, toggleID, itemInfo, list) {
+function handleToggle (listElemnt, toggleID, itemInfo, state) {
 
-  listTemplate.on("click", toggleID, function(event) {
-    var itemId = $(event.currentTarget.closest("li")).attr(itemInfo);
-    var oldItem = getItem(list, itemId);
+  listElemnt.on("click", toggleID, function(event) {
+    var itemName = $(event.currentTarget.closest("li")).attr(itemInfo);
+    var oldItem = getItem(list, itemName);
 
-    updateItem(list, itemId, {
+    updateItem(state, itemName, {
       displayName: oldItem.displayName,
       checkedOff: !oldItem.checkedOff
     });
-    makeList(list, listTemplate, itemInfo)
+    makeList(state, listElemnt, itemInfo)
   });
 }
 
 $(function() {
-  var formElement = $("#js-shopping-list-form1");
-  var listTemplate = $(".js-shopping-list");
-  var newItemId = "#js-new-item";
+  var formElement = $('.shopping-list-add');
+  var listElemnt = $(".js-shopping-list");
+  var newItemId = "#add-input";
   var removeId = ".js-shopping-item-delete";
   var itemInfo = "data-list-item-id";
   var toggleID = ".js-shopping-item-toggle"
 
-  handleAdd(formElement, newItemId, itemInfo, listTemplate, list);
-  handleDelete(formElement, removeId, itemInfo, listTemplate, list);
-  handleToggle(listTemplate,toggleID,itemInfo,list);
+  handleAdd(formElement, newItemId, itemInfo, listElemnt, state);
+  handleDelete(formElement, removeId, itemInfo, listElemnt, state);
+  handleToggle(listElemnt,toggleID,itemInfo,state);
 
 });
+
+/*
+// module-level global vars
+
+// we're using a single, global state object
+// in this app
+var state = {
+  items: []
+};
+
+
+var listItemTemplate = (
+  '<li>' +
+    '<span class="shopping-item js-shopping-item"></span>' +
+    '<div class="shopping-item-controls">' +
+      '<button class="js-shopping-item-toggle">' +
+        '<span class="button-label">check</span>' +
+      '</button>' +
+      '<button class="js-shopping-item-delete">' +
+        '<span class="button-label">delete</span>' +
+      '</button>' +
+    '</div>' +
+  '</li>'
+);
+
+
+// state management
+function addItem(state, item) {
+  state.items.push({
+    displayName: item,
+    checkedOff: false
+  });
+}
+
+function getItem(state, itemIndex) {
+  return state.items[itemIndex];
+}
+
+function deleteItem(state, itemIndex) {
+  state.items.splice(itemIndex, 1);
+}
+
+function updateItem(state, itemIndex, newItemState) {
+  state.items[itemIndex] = newItemState;
+}
+
+// DOM manipulation
+
+function renderItem(item, itemId, itemTemplate, itemDataAttr) {
+  var element = $(itemTemplate);
+  element.find('.js-shopping-item').text(item.displayName);
+  if (item.checkedOff) {
+    element.find('.js-shopping-item').addClass('shopping-item__checked');
+  }
+  element.find('.js-shopping-item-toggle')
+  element.attr(itemDataAttr, itemId);
+  return element;
+}
+
+function renderList(state, listElement, itemDataAttr) {
+  var itemsHTML = state.items.map(
+    function(item, index) {
+      return renderItem(item, index, listItemTemplate, itemDataAttr);
+  });
+  listElement.html(itemsHTML);
+}
+
+
+// Event listeners
+
+function handleItemAdds(
+  formElement, newItemIdentifier, itemDataAttr, listElement, state) {
+
+  formElement.submit(function(event) {
+    event.preventDefault();
+    var newItem = formElement.find(newItemIdentifier).val();
+    addItem(state, newItem);
+    renderList(state, listElement, itemDataAttr);
+    // reset form
+    this.reset();
+  });
+}
+
+function handleItemDeletes(
+  formElement, removeIdentifier, itemDataAttr, listElement, state) {
+
+  listElement.on('click', removeIdentifier, function(event) {
+    var itemIndex = parseInt($(this).closest('li').attr(itemDataAttr));
+    deleteItem(state, itemIndex);
+    renderList(state, listElement, itemDataAttr);
+  })
+}
+
+
+function handleItemToggles(
+  listElement, toggleIdentifier, itemDataAttr, state) {
+
+  listElement.on('click', toggleIdentifier, function(event) {
+    var itemId = $(event.currentTarget.closest('li')).attr(itemDataAttr);
+    var oldItem = getItem(state, itemId);
+
+    updateItem(state, itemId, {
+      displayName: oldItem.displayName,
+      checkedOff: !oldItem.checkedOff
+    });
+    renderList(state, listElement, itemDataAttr)
+  });
+}
+
+
+$(function() {
+  var formElement = $('#js-shopping-list-form');
+  var listElement = $('.js-shopping-list');
+
+  // from index.html -- it's the id of the input
+  // containing shopping list items
+  var newItemIdentifier = '#js-new-item';
+
+  // from `listItemTemplate` at top of this file. for each
+  // displayed shopping list item, we'll be adding a button
+  // that has this class name on it
+  var removeIdentifier = '.js-shopping-item-delete';
+
+  // we'll use this attribute to store the id of the list item
+  var itemDataAttr = 'data-list-item-id';
+
+  //
+  var toggleIdentifier = '.js-shopping-item-toggle'
+
+  handleItemAdds(
+    formElement, newItemIdentifier, itemDataAttr, listElement, state);
+  handleItemDeletes(
+    formElement, removeIdentifier, itemDataAttr, listElement, state);
+  handleItemToggles(listElement, toggleIdentifier, itemDataAttr, state);
+});
+*/
